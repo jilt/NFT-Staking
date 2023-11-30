842d463cea9371b5059c8d65bfedd942421e1f96
Laura Camellini <jilt@jeeltcraft.com>
2023-11-30 16:45:32 +0100

    Create Image.js
    creating new component to filter image IPFS links

---
+++ src/Image.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import { Route, useParams, Routes } from "react-router-dom";
+
+
+export default function Image(props) {
+    
+  const [mediaResult, setMediaResult] = useState("");
+    
+    const LoadNewMedia = () => {
+        let cond = prop.media;
+        let changing = cond.includes("cloudflare");
+        if(changing){
+            let delimiter = '/';
+            let start = 6;
+            let tokens = cond.split(delimiter).slice(4, start);
+            let resulting = tokens.join(delimiter);
+            let mediaResult = "https://nftstorage.link/ipfs/"+ resulting;
+            setMediaResult(mediaResult);
+            console.log(mediaResult);
+        } else {
+            let mediaResult = cond;
+            setMediaResult(cond);
+            console.log(cond);
+        }
+    };
+  
+  return (
+            <Card.Img variant="top" src={ mediaResult } />
+  )
+}
